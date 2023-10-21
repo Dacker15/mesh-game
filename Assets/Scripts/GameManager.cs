c028f4c3ad6818ab3728964f8a83ccefd416b7f0
Davide Barbagallo <barbang.davide@gmail.com>
2023-10-21 19:42:17 +0200

    feat: add spawn power up data

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ public sealed class GameManager : Singleton<GameManager>
     public Player player;
     public Enemy enemy;
     public List<PowerUp> spawnedPickUps;
+    public List<GameObject> powerUpSpawnPoints;
+    public List<PowerUpData> spawnablePowerUps;
     public float pickUpCooldown = 20;
     private float pickUpActualCooldown;
 
@@ -38,10 +40,10 @@ public sealed class GameManager : Singleton<GameManager>
             case PowerUpType.DAMAGE:
                 StartCoroutine(playableEntity.DamagePowerUp(powerUp.boostValue, powerUp.timeValue));
                 break;
-            case PowerUpType.COOLDOWN:
+            case PowerUpType.HEAL:
                 playableEntity.HealPowerUp(powerUp.boostValue);
                 break;
-            case PowerUpType.HEAL:
+            case PowerUpType.COOLDOWN:
                 playableEntity.CooldownPowerUp(powerUp.boostValue);
                 break;
             default:
@@ -57,20 +59,43 @@ public sealed class GameManager : Singleton<GameManager>
         pickUpActualCooldown = pickUpCooldown;
     }
 
-    private void Update()
+    private void SpawnPowerUp()
     {
-        pickUpActualCooldown -= Time.deltaTime;
-        if (pickUpActualCooldown < 0)
+        float probability = Random.Range(0f, 100f);
+        foreach (PowerUpData powerUpData in spawnablePowerUps)
         {
+            if (!(probability > powerUpData.MinProbability) || !(probability <= powerUpData.MaxProbability)) continue;
             GameObject pickUpGameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             pickUpGameObject.transform.position = new Vector3(0, 0, 0);
-            PowerUp powerUpComponent = PowerUp.CreateComponent(pickUpGameObject, 0, PowerUpType.DAMAGE, 5, 10);
+            if (powerUpData.level == 1)
+            {
+                pickUpGameObject.GetComponent<Renderer>().material.color = Color.green;   
+            }
+            if (powerUpData.level == 2)
+            {
+                pickUpGameObject.GetComponent<Renderer>().material.color = Color.yellow;   
+            }
+            if (powerUpData.level == 3)
+            {
+                pickUpGameObject.GetComponent<Renderer>().material.color = Color.red;   
+            }
+            PowerUp powerUpComponent = PowerUp.CreateComponent(pickUpGameObject, 0, powerUpData.Type, powerUpData.BoostValue, powerUpData.TimeValue);
             SphereCollider collider = pickUpGameObject.GetComponent<SphereCollider>();
             collider.isTrigger = true;
             spawnedPickUps.Add(powerUpComponent);
             Instantiate(pickUpGameObject);
             GameEvents.PowerUpSpawn(powerUpComponent);
             pickUpActualCooldown = pickUpCooldown;
+            break;
+        }
+    }
+
+    private void Update()
+    {
+        pickUpActualCooldown -= Time.deltaTime;
+        if (pickUpActualCooldown < 0)
+        {
+            SpawnPowerUp();
         }
     }
 }
---
+++ Assets/Scripts/PowerUpData.cs
@@ -0,0 +1,10 @@
+﻿[System.Serializable]
+public struct PowerUpData
+{
+    public float MinProbability;
+    public float MaxProbability;
+    public PowerUpType Type;
+    public float BoostValue;
+    public float TimeValue;
+    public int level;
+}
