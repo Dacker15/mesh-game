7dc65a30e0f759124e7fa319c6edc95075197a9b
Davide Barbagallo <barbang.davide@gmail.com>
2023-10-28 16:42:58 +0200

    feat: introduce animation

--- Assets/Scripts/Players/CubePlayer.cs
+++ Assets/Scripts/Players/CubePlayer.cs
@@ -2,17 +2,14 @@ using UnityEngine;
 
 public class CubePlayer : Player
 {
-    public int angle;
-    private bool isPlayerRotating;
-    private int maxPlayerRotation;
-    private int currentPlayerRotation = 0;
-    [SerializeField]
-    private GameObject weapon;
+    [SerializeField] private GameObject weapon;
+    [SerializeField] private AnimationClip primaryAnimation;
+    private Animator animator;
 
     public override void FirePrimary()
     {
         base.FirePrimary();
-        isPlayerRotating = true;
+        animator.Play(primaryAnimation.name);
     }
 
     public override void FireSecondary()
@@ -23,22 +20,7 @@ public class CubePlayer : Player
 
     private void Start()
     {
-        maxPlayerRotation = (360 * 3) / angle;
-    }
-
-    protected override void Update()
-    {
-        base.Update();
-        if (isPlayerRotating && currentPlayerRotation < maxPlayerRotation)
-        {
-            gameObject.transform.Rotate(new Vector3(0, -1, 0), angle);
-            currentPlayerRotation++;
-        }
-        else
-        {
-            isPlayerRotating = false;
-            currentPlayerRotation = 0;
-        }
+        animator = GetComponent<Animator>();
     }
     
 }
