c89c217b8a1349bde25ce881e8a4c12ad25e7f81
Davide Barbagallo <barbang.davide@gmail.com>
2023-11-08 18:39:08 +0100

    feat: add sphere attack controller

---
+++ Assets/Scripts/Entities/Attacks/SphereAttack.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SphereAttack : AttackController
+{
+    [SerializeField] private float primaryRotationSpeed;
+    private float primaryActualDistance;
+    [SerializeField] private float secondaryDuration;
+    private bool isSecondaryFireActive;
+    [SerializeField] private AnimationClip secondaryAnimation;
+    [SerializeField] private Animator animator;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        primaryActualDistance = 0;
+    }
+
+    protected override void FirePrimaryInput()
+    {
+        FirePrimary();
+        primaryActualDistance = 0;
+    }
+
+    protected override void FireSecondaryInput()
+    {
+        animator.Play(secondaryAnimation.name);
+        StartCoroutine(ResetSecondaryCooldown());
+    }
+
+    protected override void OnPrimaryFireUpdate()
+    {
+        if (primaryActualDistance >= 0 && primaryActualDistance <= primaryFireRadius)
+        {
+            float frameDistance = primaryRotationSpeed * Time.deltaTime;
+            transform.Translate(Vector3.forward * frameDistance);
+            transform.Rotate(Vector3.forward * frameDistance);
+            primaryActualDistance += frameDistance;
+        }
+    }
+
+    protected override void OnSecondaryFireUpdate()
+    {
+        if (isSecondaryFireActive)
+        {
+            FireSecondary();   
+        }
+    }
+
+    private IEnumerator ResetSecondaryCooldown()
+    {
+        isSecondaryFireActive = true;
+        yield return new WaitForSeconds(secondaryDuration);
+        isSecondaryFireActive = false;
+    }
+}
