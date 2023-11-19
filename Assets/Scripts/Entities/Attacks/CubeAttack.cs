6093a01bf66fe151729adfe658513ddad0dcd732
Davide Barbagallo <barbang.davide@gmail.com>
2023-11-19 17:39:03 +0100

    feat(CubePlayer): add primary attack sfx

--- Assets/Scripts/Entities/Attacks/CubeAttack.cs
+++ Assets/Scripts/Entities/Attacks/CubeAttack.cs
@@ -6,14 +6,15 @@ public class CubeAttack : AttackController
     [SerializeField] private AnimationClip primaryAnimation;
     [SerializeField] private AnimationClip secondaryAnimation;
     [SerializeField] private Animator animator;
+    [SerializeField] private AudioClip primaryRotationClip;
     [SerializeField] private AudioClip secondaryShotClip;
     private bool isPrimaryFireActive;
     
     public override void FirePrimaryInput()
     {
         inputChangeCallback(true, false, false);
-        animator.Play(primaryAnimation.name);
         StartCoroutine(WaitResetPrimaryCooldown());
+        StartCoroutine(ManagePrimaryAnimationSound());
     }
 
     public override void FireSecondaryInput()
@@ -36,6 +37,15 @@ public class CubeAttack : AttackController
 
     protected override void OnSecondaryFireUpdate() { }
 
+    private IEnumerator ManagePrimaryAnimationSound()
+    {
+        float reproductionSoundTime = primaryAnimation.length / 2;
+        animator.Play(primaryAnimation.name);
+        AudioSource.PlayClipAtPoint(primaryRotationClip, transform.position, 200);
+        yield return new WaitForSeconds(reproductionSoundTime);
+        AudioSource.PlayClipAtPoint(primaryRotationClip, transform.position, 200);
+    }
+    
     private IEnumerator WaitResetPrimaryCooldown()
     {
         isPrimaryFireActive = true;
