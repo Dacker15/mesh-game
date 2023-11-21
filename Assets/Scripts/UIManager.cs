36b306f03981d7fffd96c175e4556ff6c283e94c
Davide Barbagallo <barbang.davide@gmail.com>
2023-11-21 18:28:13 +0100

    feat(UIManager): add boost text ui

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Globalization;
 using TMPro;
 using UnityEngine;
@@ -7,13 +9,16 @@ using UnityEngine.UI;
 public class UIManager : Singleton<UIManager>
 {
     public AttackController player;
-    public AttackController enemy;
     [SerializeField] private TextMeshProUGUI playerPrimaryCooldownText;
     [SerializeField] private TextMeshProUGUI playerSecondaryCooldownText;
     [SerializeField] private Slider playerHealth;
     [SerializeField] private Slider enemyHealth;
     [SerializeField] private TextMeshProUGUI playerHealthText;
     [SerializeField] private TextMeshProUGUI enemyHealthText;
+    private List<PowerUp> playerBoosts;
+    [SerializeField] private TextMeshProUGUI playerBoostText;
+    private List<PowerUp> enemyBoosts;
+    [SerializeField] private TextMeshProUGUI enemyBoostText;
     [SerializeField] private TextMeshProUGUI matchTimeText;
 
     private string ParseCooldown(float value, float threshold)
@@ -21,17 +26,62 @@ public class UIManager : Singleton<UIManager>
         return Math.Max(threshold, Math.Ceiling(value)).ToString(CultureInfo.InvariantCulture);
     }
     
+    private string ParseBoosts(List<PowerUp> powerUps)
+    {
+        string result = "";
+        foreach (PowerUp powerUp in powerUps)
+        {
+            string boostValue = powerUp.boostValue.ToString(CultureInfo.InvariantCulture).Replace(',', '.');
+            string rawType = powerUp.type.ToString();
+            string type = rawType.Substring(0, 1).ToUpper() + rawType.Substring(1).ToLower();
+            result += $"{boostValue} x {type}" + "\n";
+        }
+
+        return result;
+    }
+    
+    private IEnumerator RemoveBoost(List<PowerUp> powerUps, PowerUp powerUp)
+    {
+        yield return new WaitForSeconds(powerUp.timeValue);
+        powerUps.Remove(powerUp);
+    }
+
+    private void HandlePowerUpPick(PowerUp powerUp, Collider other)
+    {
+        Debug.LogFormat("PowerUp {0} picked by {1} with time {2}", powerUp.type, other.tag, powerUp.timeValue);
+        if (powerUp.timeValue == 0) return;
+        
+        List<PowerUp> powerUps = other.CompareTag("Player") ? playerBoosts : enemyBoosts;
+        powerUps.Add(powerUp);
+        StartCoroutine(RemoveBoost(powerUps, powerUp));
+    }
+    
+    protected override void Awake()
+    {
+        base.Awake();
+        GameEvents.onPowerUpPick += HandlePowerUpPick;
+        playerBoosts = new List<PowerUp>();
+        enemyBoosts = new List<PowerUp>();
+    }
+    
     private void Update()
     {
         playerPrimaryCooldownText.text = ParseCooldown(player.primaryActualCooldown, 0);
         playerSecondaryCooldownText.text = ParseCooldown(player.secondaryActualCooldown, 0);
         playerHealth.value = GameManager.Instance.player.health / 100;
-        enemyHealth.value = GameManager.Instance.player.health / 100;
+        enemyHealth.value = GameManager.Instance.enemy.health / 100;
         playerHealthText.text = GameManager.Instance.player.health.ToString(CultureInfo.InvariantCulture);
         enemyHealthText.text = GameManager.Instance.enemy.health.ToString(CultureInfo.InvariantCulture);
+        playerBoostText.text = ParseBoosts(playerBoosts);
+        enemyBoostText.text = ParseBoosts(enemyBoosts);
 
         string minutes = Math.Floor(GameManager.Instance.matchTime / 60).ToString(CultureInfo.InvariantCulture);
         string seconds = Math.Floor(GameManager.Instance.matchTime % 60).ToString(CultureInfo.InvariantCulture).PadLeft(2, '0');
         matchTimeText.text = $"{minutes}:{seconds}";
+    }
+    
+    private void OnDestroy()
+    {
+        GameEvents.onPowerUpPick -= HandlePowerUpPick;
     }
 }
